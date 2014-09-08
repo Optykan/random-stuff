a9310d6e541133d792ec4aad3a80e7855dd7d3da
syang0525 <syang0525@gmail.com>
2014-09-08 13:45:04 -0400

    Transitions for dropdown

--- dist/js/misc.js
+++ dist/js/misc.js
@@ -8,13 +8,13 @@ function hide(){
 }
 function menu(){
 	if($.cookie('toggle')=="0"){
-		//document.getElementById('nav-content').style.height="50px";
+		//document.getElementById('nav-content').style.height="auto";
 		document.getElementById('nav-content').style.opacity="1";
 		$.cookie('toggle', '1', { expires: 7, path: '/'});
 		console.log("1");
 	}
 	else{
-		//document.getElementById('nav-content').style.height="300px";
+		//document.getElementById('nav-content').style.height="0px";
 		document.getElementById('nav-content').style.opacity="0";
 	 	$.cookie('toggle', '0', { expires: 7, path: '/'});
 	 	console.log($.cookie('toggle'));
